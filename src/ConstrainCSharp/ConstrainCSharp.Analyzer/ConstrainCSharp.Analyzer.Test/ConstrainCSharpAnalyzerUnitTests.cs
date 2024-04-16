aa42974980ed97389d0f5f5c1542473a1a7ca0e8
WANJI\lijie1 <lijie1@wanji.net.cn>
2024-04-16 16:48:23 +0800

    Init.

---
+++ src/ConstrainCSharp/ConstrainCSharp.Analyzer/ConstrainCSharp.Analyzer.Test/ConstrainCSharpAnalyzerUnitTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = ConstrainCSharp.Analyzer.Test.CSharpCodeFixVerifier<
+    ConstrainCSharp.Analyzer.ConstrainCSharpAnalyzerAnalyzer,
+    ConstrainCSharp.Analyzer.ConstrainCSharpAnalyzerCodeFixProvider>;
+
+namespace ConstrainCSharp.Analyzer.Test
+{
+    [TestClass]
+    public class ConstrainCSharpAnalyzerUnitTest
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class {|#0:TypeName|}
+        {   
+        }
+    }";
+
+            var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic("ConstrainCSharpAnalyzer").WithLocation(0).WithArguments("TypeName");
+            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+        }
+    }
+}
---
+++ src/ConstrainCSharp/ConstrainCSharp/Attributes/OnlyOneAssignmentStatementAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ConstrainCSharp.Attributes {
+    /// <summary>
+    /// 使私有字段只存在一个赋值语句
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class OnlyOneAssignmentStatementAttribute : Attribute { }
+}
