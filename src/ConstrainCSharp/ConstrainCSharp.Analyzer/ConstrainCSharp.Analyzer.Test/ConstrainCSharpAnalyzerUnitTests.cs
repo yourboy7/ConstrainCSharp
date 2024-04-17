d01c57dfef2fdab75504966be1c21279c4f0144d
yourboy77 <243864016@qq.com>
2024-04-18 00:17:50 +0800

    Add OnlyOneAssignmentStatementAttribute, realize the analyzer and add the unit tests.

--- src/ConstrainCSharp/ConstrainCSharp.Analyzer/ConstrainCSharp.Analyzer.Test/ConstrainCSharpAnalyzerUnitTests.cs
+++ src/ConstrainCSharp/ConstrainCSharp.Analyzer/ConstrainCSharp.Analyzer.Test/ConstrainCSharpAnalyzerUnitTests.cs
@@ -1,27 +1,23 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.IO;
+using ConstrainCSharp.Attributes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
-using VerifyCS = ConstrainCSharp.Analyzer.Test.CSharpCodeFixVerifier<
-    ConstrainCSharp.Analyzer.ConstrainCSharpAnalyzerAnalyzer,
-    ConstrainCSharp.Analyzer.ConstrainCSharpAnalyzerCodeFixProvider>;
+using Microsoft.CodeAnalysis.Testing;
+using VerifyCS =
+    ConstrainCSharp.Analyzer.Test.CSharpCodeFixVerifier<
+        ConstrainCSharp.Analyzer.ConstrainCSharpAnalyzerAnalyzer,
+        ConstrainCSharp.Analyzer.ConstrainCSharpAnalyzerCodeFixProvider>;
 
-namespace ConstrainCSharp.Analyzer.Test
-{
+namespace ConstrainCSharp.Analyzer.Test {
     [TestClass]
-    public class ConstrainCSharpAnalyzerUnitTest
-    {
-        //No diagnostics expected to show up
+    public class ConstrainCSharpAnalyzerUnitTest {
+        /// <summary>
+        /// Verifying that there is only one assignment statement where a private field is declared does not trigger diagnostics
+        /// 验证只存在位于声明私有字段处的一条赋值语句时不会触发诊断
+        /// </summary>
         [TestMethod]
-        public async Task TestMethod1()
-        {
-            var test = @"";
-
-            await VerifyCS.VerifyAnalyzerAsync(test);
-        }
-
-        //Diagnostic and CodeFix both triggered and checked for
-        [TestMethod]
-        public async Task TestMethod2()
-        {
+        public async Task
+            OnlyOneAssignmentStatementInPrivateField_NoDiagnostic() {
             var test = @"
     using System;
     using System.Collections.Generic;
@@ -29,31 +25,107 @@ namespace ConstrainCSharp.Analyzer.Test
     using System.Text;
     using System.Threading.Tasks;
     using System.Diagnostics;
+    using ConstrainCSharp.Attributes;
 
-    namespace ConsoleApplication1
-    {
-        class {|#0:TypeName|}
-        {   
+    namespace TestConstrainCSharp {
+
+        public class Boy {
+            [OnlyOneAssignmentStatement] 
+            private int _age = 11;
+
+            public int GetAge() {
+               return _age;
+            }
         }
     }";
+            await new VerifyCS.Test {
+                ReferenceAssemblies =
+                    new ReferenceAssemblies("net8.0",
+                        new PackageIdentity("Microsoft.NETCore.App.Ref",
+                            "8.0.0"), Path.Combine("ref", "net8.0")),
+                TestState = {
+                    Sources = { test },
+                    AdditionalReferences = {
+                        typeof(OnlyOneAssignmentStatementAttribute).Assembly
+                            .Location
+                    }
+                }
+            }.RunAsync();
+        }
 
-            var fixtest = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
+        /// <summary>
+        /// Verifying that there is only one assignment statement at the local scope does not trigger diagnostics
+        /// 验证只存在位于局部作用域处的一条赋值语句时不会触发诊断
+        /// </summary>
+        [TestMethod]
+        public async Task
+            OnlyOneAssignmentStatementInLocalScope_NoDiagnostic() {
+            var test = @"
+    using ConstrainCSharp.Attributes;
 
-    namespace ConsoleApplication1
-    {
-        class TYPENAME
-        {   
+    namespace TestConstrainCSharp {
+
+        public class Boy {
+            [OnlyOneAssignmentStatement] private int _age;
+
+            public void SetAge(int age) {
+                _age = age;
+            }
         }
     }";
+            await new VerifyCS.Test
+            {
+                ReferenceAssemblies =
+                    new ReferenceAssemblies("net8.0",
+                        new PackageIdentity("Microsoft.NETCore.App.Ref",
+                            "8.0.0"), Path.Combine("ref", "net8.0")),
+                TestState = {
+                    Sources = { test },
+                    AdditionalReferences = {
+                        typeof(OnlyOneAssignmentStatementAttribute).Assembly
+                            .Location
+                    }
+                }
+            }.RunAsync();
+        }
 
-            var expected = VerifyCS.Diagnostic("ConstrainCSharpAnalyzer").WithLocation(0).WithArguments("TypeName");
-            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+        /// <summary>
+        /// Verify that diagnostics are triggered when there are multiple assignment statements
+        /// 验证当存在多条赋值语句时会触发诊断
+        /// </summary>
+        [TestMethod]
+        public async Task MultipleAssignmentStatements_Diagnostic() {
+            var test = @"
+    using ConstrainCSharp.Attributes;
+
+    namespace TestConstrainCSharp {
+
+        public class Boy {
+            [OnlyOneAssignmentStatement] 
+            private int _age = 11;
+
+            public void SetAge(int age) {
+                {|#0:_age = age|};
+            }
+        }
+    }";
+            var expected = VerifyCS.Diagnostic("ConstrainCSharpAnalyzer")
+                .WithLocation(0).WithArguments("_age");
+            await new VerifyCS.Test
+            {
+                ReferenceAssemblies =
+                    new ReferenceAssemblies("net8.0",
+                        new PackageIdentity("Microsoft.NETCore.App.Ref",
+                            "8.0.0"), Path.Combine("ref", "net8.0")),
+                TestState = {
+                    Sources = { test },
+                    ExpectedDiagnostics = { expected },
+                    AdditionalReferences = {
+                        typeof(OnlyOneAssignmentStatementAttribute).Assembly
+                            .Location
+                    }
+                }
+            }.RunAsync();
         }
     }
 }
--- src/ConstrainCSharp/ConstrainCSharp.Analyzer/ConstrainCSharp.Analyzer/ConstrainCSharpAnalyzerAnalyzer.cs
+++ src/ConstrainCSharp/ConstrainCSharp.Analyzer/ConstrainCSharp.Analyzer/ConstrainCSharpAnalyzerAnalyzer.cs
@@ -8,47 +8,102 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 
-namespace ConstrainCSharp.Analyzer
-{
+namespace ConstrainCSharp.Analyzer {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class ConstrainCSharpAnalyzerAnalyzer : DiagnosticAnalyzer
-    {
+    public class ConstrainCSharpAnalyzerAnalyzer : DiagnosticAnalyzer {
         public const string DiagnosticId = "ConstrainCSharpAnalyzer";
+
+        /// <summary>
+        /// 使私有字段只存在一个赋值语句
+        /// </summary>
+        private const string OnlyOneAssignmentStatementAttribute =
+            "OnlyOneAssignmentStatement";
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
+        private static readonly LocalizableString Title =
+            new LocalizableResourceString(nameof(Resources.AnalyzerTitle),
+                Resources.ResourceManager, typeof(Resources));
 
-        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        private static readonly LocalizableString MessageFormat =
+            new LocalizableResourceString(
+                nameof(Resources.AnalyzerMessageFormat),
+                Resources.ResourceManager, typeof(Resources));
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+        private static readonly LocalizableString Description =
+            new LocalizableResourceString(nameof(Resources.AnalyzerDescription),
+                Resources.ResourceManager, typeof(Resources));
 
-        public override void Initialize(AnalysisContext context)
-        {
-            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
-            context.EnableConcurrentExecution();
+        private const string Category = "Usage";
 
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        private static readonly DiagnosticDescriptor Rule =
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat,
+                Category, DiagnosticSeverity.Error, isEnabledByDefault: true,
+                description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor>
+            SupportedDiagnostics {
+            get { return ImmutableArray.Create(Rule); }
         }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
-        {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+        public override void Initialize(AnalysisContext context) {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags
+                .None);
+            context.EnableConcurrentExecution();
 
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
-            {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+            context.RegisterSyntaxNodeAction(AnalyzeNode,
+                SyntaxKind.SimpleAssignmentExpression);
+        }
 
-                context.ReportDiagnostic(diagnostic);
-            }
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context) {
+            // 取局部变量名
+            var assignmentExpressionSyntaxNode =
+                (AssignmentExpressionSyntax)context.Node;
+            var localVariableName =
+                (assignmentExpressionSyntaxNode.Left as IdentifierNameSyntax)
+                ?.Identifier.ValueText;
+
+            // 检查局部变量名是否是标记上ReadonlyAfterAssignment特性的私有字段名,是则取出字段声明语法节点,如果不是则返回
+            var syntaxTree = assignmentExpressionSyntaxNode.SyntaxTree;
+            var rootNode = syntaxTree.GetCompilationUnitRoot();
+
+            var targetFieldDeclarationSyntaxNode = rootNode.DescendantNodes()
+                .OfType<AttributeSyntax>()
+                .Where(a =>
+                    (a.Name as IdentifierNameSyntax)?.Identifier.ValueText ==
+                    OnlyOneAssignmentStatementAttribute)
+                .Select(a => (FieldDeclarationSyntax)a.Parent.Parent)
+                .SingleOrDefault(f =>
+                    f.Declaration.Variables.Any(v =>
+                        v.Identifier.ValueText == localVariableName));
+            if (targetFieldDeclarationSyntaxNode is null ||
+                !targetFieldDeclarationSyntaxNode.Modifiers.Any(SyntaxKind
+                    .PrivateKeyword))
+                return;
+
+            // 检查标记上ReadonlyAfterAssignment特性的同名字段是否被赋值
+            var hasAssignedInField = targetFieldDeclarationSyntaxNode
+                .Declaration.Variables
+                .Single(v => v.Identifier.ValueText == localVariableName)
+                .DescendantNodes()
+                .Any(n => n.IsKind(SyntaxKind.EqualsValueClause));
+
+            // 计算目标局部变量赋值次数
+            var localVariableAssignedCount = rootNode.DescendantNodes()
+                .OfType<AssignmentExpressionSyntax>()
+                .Where(a => a.IsKind(SyntaxKind.SimpleAssignmentExpression))
+                .Count(a =>
+                    ((IdentifierNameSyntax)a.Left).Identifier.ValueText ==
+                    localVariableName);
+
+            // 计算总赋值次数
+            var totalAssignedCount = localVariableAssignedCount +
+                (hasAssignedInField ? 1 : 0);
+
+            // 当总赋值次数超过一次则报告错误
+            if (totalAssignedCount > 1)
+                context.ReportDiagnostic(Diagnostic.Create(Rule,
+                    assignmentExpressionSyntaxNode.GetLocation(), localVariableName));
         }
     }
 }
