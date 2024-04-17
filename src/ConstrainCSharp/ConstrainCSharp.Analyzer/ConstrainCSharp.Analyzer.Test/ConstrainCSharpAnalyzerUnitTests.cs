1589677a82ff1a43b2d5a56154ecbde6f6aaea52
yourboy77 <243864016@qq.com>
2024-04-18 00:45:11 +0800

    clean

--- src/ConstrainCSharp/ConstrainCSharp.Analyzer/ConstrainCSharp.Analyzer.Test/ConstrainCSharpAnalyzerUnitTests.cs
+++ src/ConstrainCSharp/ConstrainCSharp.Analyzer/ConstrainCSharp.Analyzer.Test/ConstrainCSharpAnalyzerUnitTests.cs
@@ -19,12 +19,6 @@ namespace ConstrainCSharp.Analyzer.Test {
         public async Task
             OnlyOneAssignmentStatementInPrivateField_NoDiagnostic() {
             var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
     using ConstrainCSharp.Attributes;
 
     namespace TestConstrainCSharp {
--- src/ConstrainCSharp/ConstrainCSharp/Attributes/OnlyOneAssignmentStatementAttribute.cs
+++ src/ConstrainCSharp/ConstrainCSharp/Attributes/OnlyOneAssignmentStatementAttribute.cs
@@ -2,6 +2,7 @@
 
 namespace ConstrainCSharp.Attributes {
     /// <summary>
+    /// Make the private field only have one assignment statement
     /// 使私有字段只存在一个赋值语句
     /// </summary>
     [AttributeUsage(AttributeTargets.Field)]
